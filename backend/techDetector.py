0524da732f7d73d4d7bddac3dcab842bff784f23
Ashwin Harish <ashwinsh22@gmail.com>
2026-01-08 12:33:30 +0000

    added health check for backend

--- backend/techDetector.py
+++ backend/techDetector.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-
+import time
 app = FastAPI()
 
 class VideoRequest(BaseModel):
     title: str
+
+start_time = time.time()
+
+@app.get("/health")
+def health():
+    return {
+        "status": "ok",
+        "uptime": time.time() - start_time 
+    }
 
 @app.get("/")
 def health_check():
@@ -20,7 +29,7 @@ def classify_video(data: VideoRequest):
         "programming", "coding", "development", "system design",
         "optimization", "ai", "machine learning"
     ]
-
+    #this is still not using AI. will implement it in next few days
     is_tech = any(keyword in title for keyword in tech_keywords)
 
     return {
